69eb80ce1a26649d74af54b96b3e263f6fc0527c
kuttapa <arrliver@gmail.com>
2024-10-12 19:41:57 +0700

    feat: integrate mongodb and create Car module

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require('./db/connect.js')
+const dotenv = require('dotenv')
+const Car = require('./models/Car.js')
+dotenv.config()
 
 const app = express();
 
-let products = [
-    { id:1 ,name:"Shirt",price:500},
-    { id:2 ,name:"Pants",price:800},
-    { id:3 ,name:"Watch",price:1000},
-]
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 
-app.get("/products", (req, res) => {
-    res.status(200).json(products)
+app.use(cors())
+app.get("/cars", async (req, res) => {
+    const cars = await Car.find();
+    res.status(200).json(cars)
 });
 
 // Middleware for handling routes that do not exist
@@ -18,8 +21,16 @@ app.use((req, res, next) => {
     res.status(404).json({ message: "Route does not exist" }); 
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
-
+const startServer = async () => {
+    try {
+      await connectDB(MONGO_URL);
+      app.listen(PORT, () => {
+        console.log(`server is running on port : ${PORT}`);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  startServer();
 
