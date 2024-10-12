88ce331b3048e6d38462ae5c79e4806c75b9c08b
kuttapa <arrliver@gmail.com>
2024-10-13 03:30:16 +0700

    refactor: added prefix route

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const MONGO_URL = process.env.MONGO_URL;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.status(200).json({ msg: "Welcome to our API" });
 });
 
-app.get("/cars", async (req, res) => {
+app.get("/api/cars", async (req, res) => {
   const cars = await Car.find();
   res.status(200).json(cars);
 });
